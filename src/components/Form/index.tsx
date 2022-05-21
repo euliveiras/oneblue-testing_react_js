a2c6606382c1dffc8e2062db731ca98e90f8c325
matheus oliveira <matheus.skm@hotmail.com>
2022-05-21 12:57:35 -0300

    style: changed toast message when the api throw an error

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -15,15 +15,19 @@ import { useState } from "react";
 import { FiUser, FiLock } from "react-icons/fi";
 import { CircleComponent } from "../Circle";
 import { FormInput } from "./FormInput";
+import { api } from "../../utils/axios";
 
 type FormProps = {
   formType: "login" | "cadaster";
 };
 
 const logInSchema = Yup.object().shape({
-  user: Yup.string().required("Nome Obrigatório"),
+  user: Yup.string()
+    .required("Nome Obrigatório")
+    .max(40, "Máximo de 40 caracteres"),
   password: Yup.string()
     .min(6, "Mínimo de seis dígitos")
+    .max(40, "Máximo de 40 caracteres")
     .required("Senha obrigatória"),
 });
 
@@ -34,21 +38,28 @@ export const Form = ({ formType }: FormProps) => {
   if (formType === "login") {
     return (
       <>
-        <Box>
-          <Box
-            position={"relative"}
-            w={"sm"}
-            h={"md"}
-            backgroundColor="whiteAlpha.900"
-            borderRadius={"base"}
-            boxShadow={"lg"}
-          >
-            <CircleComponent />
-            <Formik
-              initialValues={{ user: "", password: "" }}
-              onSubmit={(data) => {
-                console.log(data);
-                setIsSubmitting(true);
+        <Box
+          position={"relative"}
+          w={"sm"}
+          h={"md"}
+          backgroundColor="whiteAlpha.900"
+          borderRadius={"base"}
+          boxShadow={"lg"}
+        >
+          <CircleComponent />
+          <Formik
+            initialValues={{ user: "", password: "" }}
+            onSubmit={async (data) => {
+              setIsSubmitting(true);
+              try {
+                const response = await api.post("/login", {
+                  name: data.user,
+                  password: data.password,
+                });
+                console.log(response);
+              } catch (err) {
+                console.log(err);
+                setIsSubmitting(false);
                 toast({
                   position: "top-right",
                   title: "Erro",
@@ -57,67 +68,60 @@ export const Form = ({ formType }: FormProps) => {
                   duration: 10000,
                   isClosable: true,
                 });
-              }}
-              validationSchema={logInSchema}
-            >
-              {({ handleSubmit, errors, touched }) => (
-                <VStack
-                  as="form"
-                  onSubmit={handleSubmit}
-                  spacing={"4"}
-                  mx="10%"
+              }
+            }}
+            validationSchema={logInSchema}
+          >
+            {({ handleSubmit, errors, touched }) => (
+              <VStack as="form" onSubmit={handleSubmit} spacing={"4"} mx="10%">
+                {/* error unsolved in types of onSubmit */}
+                <Text fontSize="2xl" mt="30%" fontWeight={"bold"}>
+                  Log in
+                </Text>
+                <FormControl isInvalid={!!errors.user && touched.user}>
+                  <Field
+                    as={FormInput}
+                    name="user"
+                    type="text"
+                    icon={FiUser}
+                    placeholder="name"
+                  />
+                  <FormErrorMessage>{errors.user}</FormErrorMessage>
+                </FormControl>
+                <FormControl isInvalid={!!errors.password && touched.password}>
+                  <Field
+                    as={FormInput}
+                    name="password"
+                    type="password"
+                    placeholder="password"
+                    icon={FiLock}
+                  />
+                  <FormErrorMessage>{errors.password}</FormErrorMessage>
+                </FormControl>
+                <Button
+                  isLoading={isSubmitting}
+                  loadingText="Entrando"
+                  colorScheme={"blue"}
+                  variant={"solid"}
+                  w="80%"
+                  h="3.2rem"
+                  type="submit"
+                  borderRadius={"base"}
                 >
-                  {/* error unsolved in types of onSubmit */}
-                  <Text fontSize="2xl" mt="30%" fontWeight={"bold"}>
-                    Log in
-                  </Text>
-                  <FormControl isInvalid={!!errors.user && touched.user}>
-                    <Field
-                      as={FormInput}
-                      name="user"
-                      type="text"
-                      icon={FiUser}
-                      placeholder="name"
-                    />
-                    <FormErrorMessage>{errors.user}</FormErrorMessage>
-                  </FormControl>
-                  <FormControl
-                    isInvalid={!!errors.password && touched.password}
+                  entrar
+                </Button>
+                <Link href="/cadaster" passHref>
+                  <ChakraLink
+                    color={"gray.500"}
+                    textDecoration="none"
+                    _hover={{ color: "gray.800" }}
                   >
-                    <Field
-                      as={FormInput}
-                      name="password"
-                      type="password"
-                      placeholder="password"
-                      icon={FiLock}
-                    />
-                    <FormErrorMessage>{errors.password}</FormErrorMessage>
-                  </FormControl>
-                  <Button
-                    isLoading={isSubmitting}
-                    loadingText="Entrando"
-                    colorScheme={"blue"}
-                    variant={"solid"}
-                    w="80%"
-                    h="3.2rem"
-                    type="submit"
-                    borderRadius={"base"}
-                  >
-                    entrar
-                  </Button>
-                  <Link href="/cadaster" passHref>
-                    <ChakraLink
-                      color={"gray.500"}
-                      textDecoration="none"
-                      _hover={{ color: "gray.800" }}
-                    >
-                      criar conta
-                    </ChakraLink>
-                  </Link>
-                </VStack>
-              )}
-            </Formik>
-          </Box>
+                    criar conta
+                  </ChakraLink>
+                </Link>
+              </VStack>
+            )}
+          </Formik>
         </Box>
       </>
     );
@@ -126,90 +130,96 @@ export const Form = ({ formType }: FormProps) => {
   if (formType === "cadaster") {
     return (
       <>
-        <Box>
-          <Box
-            position={"relative"}
-            w={"sm"}
-            h={"md"}
-            backgroundColor="whiteAlpha.900"
-            borderRadius={"base"}
-            boxShadow={"lg"}
-          >
-            <CircleComponent />
-            <Formik
-              initialValues={{ user: "", password: "" }}
-              onSubmit={(data) => {
-                console.log(data);
-                setIsSubmitting(true);
+        <Box
+          position={"relative"}
+          w={"sm"}
+          h={"md"}
+          backgroundColor="whiteAlpha.900"
+          borderRadius={"base"}
+          boxShadow={"lg"}
+        >
+          <CircleComponent />
+          <Formik
+            initialValues={{ user: "", password: "" }}
+            onSubmit={async (data) => {
+              try {
+                const response = await api.post("/user/cadaster", {
+                  name: data.user,
+                  password: data.password,
+                });
                 toast({
                   position: "top-right",
-                  title: "Conta criada",
-                  description: "A conta foi criada com sucesso",
+                  title: "Cadastro",
+                  description: "Conta criada",
                   status: "success",
                   duration: 10000,
                   isClosable: true,
                 });
-              }}
-              validationSchema={logInSchema}
-            >
-              {({ handleSubmit, errors, touched }) => (
-                <VStack
-                  as="form"
-                  onSubmit={handleSubmit}
-                  spacing={"4"}
-                  mx="10%"
+              } catch (err: any) {
+                const message = err.response.data.why
+                setIsSubmitting(false);
+                toast({
+                  position: "top-right",
+                  title: "Error",
+                  description: message,
+                  status: "error",
+                  duration: 10000,
+                  isClosable: true,
+                });
+              }
+            }}
+            validationSchema={logInSchema}
+          >
+            {({ handleSubmit, errors, touched }) => (
+              <VStack as="form" onSubmit={handleSubmit} spacing={"4"} mx="10%">
+                {/* error unsolved in types of onSubmit */}
+                <Text fontSize="2xl" mt="30%" fontWeight={"bold"}>
+                  Cadastrar
+                </Text>
+                <FormControl isInvalid={!!errors.user && touched.user}>
+                  <Field
+                    as={FormInput}
+                    name="user"
+                    type="text"
+                    icon={FiUser}
+                    placeholder="name"
+                  />
+                  <FormErrorMessage>{errors.user}</FormErrorMessage>
+                </FormControl>
+                <FormControl isInvalid={!!errors.password && touched.password}>
+                  <Field
+                    as={FormInput}
+                    name="password"
+                    type="password"
+                    placeholder="password"
+                    icon={FiLock}
+                  />
+                  <FormErrorMessage>{errors.password}</FormErrorMessage>
+                </FormControl>
+                <Button
+                  isLoading={isSubmitting}
+                  loadingText="Cadastrando"
+                  colorScheme={"blue"}
+                  variant={"solid"}
+                  w="80%"
+                  h="3.2rem"
+                  type="submit"
+                  borderRadius={"base"}
                 >
-                  {/* error unsolved in types of onSubmit */}
-                  <Text fontSize="2xl" mt="30%" fontWeight={"bold"}>
-                    Cadastrar
-                  </Text>
-                  <FormControl isInvalid={!!errors.user && touched.user}>
-                    <Field
-                      as={FormInput}
-                      name="user"
-                      type="text"
-                      icon={FiUser}
-                      placeholder="name"
-                    />
-                    <FormErrorMessage>{errors.user}</FormErrorMessage>
-                  </FormControl>
-                  <FormControl
-                    isInvalid={!!errors.password && touched.password}
+                  Cadastrar
+                </Button>
+                <Link href="/" passHref>
+                  <ChakraLink
+                    color={"gray.500"}
+                    textDecoration="none"
+                    _hover={{ color: "gray.800" }}
                   >
-                    <Field
-                      as={FormInput}
-                      name="password"
-                      type="password"
-                      placeholder="password"
-                      icon={FiLock}
-                    />
-                    <FormErrorMessage>{errors.password}</FormErrorMessage>
-                  </FormControl>
-                  <Button
-                    isLoading={isSubmitting}
-                    loadingText="Cadastrando"
-                    colorScheme={"blue"}
-                    variant={"solid"}
-                    w="80%"
-                    h="3.2rem"
-                    type="submit"
-                    borderRadius={"base"}
-                  >
-                    entrar
-                  </Button>
-                  <Link href="/" passHref>
-                    <ChakraLink
-                      color={"gray.500"}
-                      textDecoration="none"
-                      _hover={{ color: "gray.800" }}
-                    >
-                      já tem uma conta?
-                    </ChakraLink>
-                  </Link>
-                </VStack>
-              )}
-            </Formik>
-          </Box>
+                    já tem uma conta?
+                  </ChakraLink>
+                </Link>
+              </VStack>
+            )}
+          </Formik>
         </Box>
       </>
     );
